59000463edcb6300197ea4b2d46b18c8a07bced8
AhmedReFu <mdnazmulkhanfeni@gmail.com>
2022-05-27 13:21:39 +0600

    login page create

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Footer from './Pages/Shared/Footer/Footer';
 import Blogs from './Pages/Blogs/Blogs';
 import NotFound from './Pages/NotFound/NotFound';
 import About from './Pages/About/About';
+import Login from './Pages/Login/Login/Login';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path='/' element={<Home></Home>} ></Route>
         <Route path='/blogs' element={<Blogs></Blogs>} ></Route>
         <Route path='/about' element={<About></About>} ></Route>
+        <Route path='/login' element={<Login></Login>} ></Route>
         <Route path='*' element={<NotFound></NotFound>} ></Route>
       </Routes>
       <Footer></Footer>
